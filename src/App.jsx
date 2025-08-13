3480305774de1d1c443590b4cc0fabc9f1394433
apaarmeet <apaarmeet5000@gmail.com>
2025-08-14 01:34:04 +0530

    add the showBalance

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,11 @@ import { clusterApiUrl } from "@solana/web3.js";
 // Default styles that can be overridden by your app
 import "@solana/wallet-adapter-react-ui/styles.css";
 import  {Airdrop} from "./Airdrop";
+import { ShowBalance } from "./ShowBalance";
 
 function App() {
   return (
-    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/NzcjxTamdyKCgFwE0xzsi"}>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
 
@@ -28,6 +29,7 @@ function App() {
             <WalletDisconnectButton/>
             Hi there
             <Airdrop/>
+            <ShowBalance/>
           
           </div>
 
